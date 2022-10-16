ddf6918ec450573c65c394d288095542be026fda
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-16 23:12:18 +0530

    added comments on server file

--- Pc Server/Server.java
+++ Pc Server/Server.java
@@ -1,11 +1,8 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,38 +10,51 @@ import java.util.Objects;
 
 public class Server {
     ServerSocket serverSocket;
-    static int x;
-    static int y;
 
     Server(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
 
+    /**
+     * Method for start server and listen for clients
+     */
     public void startServer() {
         try {
+            // getting custom cursor image from resources
             Image cursor = ImageIO.read(Objects.requireNonNull(getClass().getResource("cursor.png")));
 
+            //listening for clients
             while (!serverSocket.isClosed()) {
                 Socket socket = serverSocket.accept();
                 socket.setTcpNoDelay(true);
                 System.out.println("Client connected");
+
+                //start new thread for every client connect
                 new Thread(() -> {
                     while (socket.isConnected()) {
                         try {
                             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                            Robot robot = new Robot();
-                            //send screen image
-                            BufferedImage image = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
 
+                            //capture screen
+                            BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+
+                            //draw custom cursor on screen captured image
                             Graphics2D graphics2D = image.createGraphics();
                             int mouseX = MouseInfo.getPointerInfo().getLocation().x;
                             int mouseY = MouseInfo.getPointerInfo().getLocation().y;
                             graphics2D.drawImage(cursor, mouseX, mouseY, null);
                             graphics2D.dispose();
 
+                            //convert image to byte array
                             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+                            //write image to byte array
                             ImageIO.write(image, "png", byteArrayOutputStream);
+
+                            //send image size to client
                             dataOutputStream.writeInt(byteArrayOutputStream.size());
+
+                            //send image to client
                             dataOutputStream.write(byteArrayOutputStream.toByteArray());
                             dataOutputStream.flush();
 
@@ -75,7 +85,6 @@ public class Server {
         try {
             ServerSocket serverSocket = new ServerSocket(8080);
             Server server = new Server(serverSocket);
-
 
             String ip = InetAddress.getLocalHost().getHostAddress();
             System.out.println("Server is running on " + ip);
