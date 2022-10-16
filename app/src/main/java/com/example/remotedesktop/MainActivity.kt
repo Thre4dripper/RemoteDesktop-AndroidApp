2860e7e63bb903706dbf7780db03d45f9e190352
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-16 22:53:04 +0530

    implemented ip dialog for user input

--- app/src/main/java/com/example/remotedesktop/MainActivity.kt
+++ app/src/main/java/com/example/remotedesktop/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.remotedesktop
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.remotedesktop.databinding.ActivityMainBinding
+import com.example.remotedesktop.databinding.IpDialogLayoutBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.io.DataInputStream
 import java.net.Socket
 
@@ -27,13 +30,32 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility =
             (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
 
-        connectToServer()
+        getIp()
+    }
+
+    private fun getIp() {
+        //custom dialog layout
+        val dialogBinding = DataBindingUtil.inflate<IpDialogLayoutBinding>(
+            LayoutInflater.from(this), R.layout.ip_dialog_layout, null, false
+        )
+        dialogBinding.ipEditText.setText(viewModel.ip)
+        // Create an alert dialog to get ip from user
+        MaterialAlertDialogBuilder(this).setTitle("Enter IP").setView(dialogBinding.root)
+            .setPositiveButton("Connect") { _, _ ->
+                val ip = dialogBinding.ipEditText.text.toString()
+                viewModel.ip = ip
+
+                //now start socket connection
+                connectToServer()
+            }.setNegativeButton("Exit") { _, _ ->
+                finish()
+            }.setCancelable(false).show()
     }
 
     private fun connectToServer() {
         socketThread = Thread {
             try {
-                viewModel.socket = Socket("192.168.0.110", 8080)
+                viewModel.socket = Socket(viewModel.ip, 8080)
                 runOnUiThread {
                     Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
                 }
