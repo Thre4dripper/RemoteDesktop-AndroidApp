f2eba98a4d701eee142aeb57b56560cf46ad4da7
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-16 23:05:54 +0530

    added comments and handled server disconnection

--- app/src/main/java/com/example/remotedesktop/MainActivity.kt
+++ app/src/main/java/com/example/remotedesktop/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
-    private lateinit var socketThread: Thread
+    private var socketThread: Thread? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +33,9 @@ class MainActivity : AppCompatActivity() {
         getIp()
     }
 
+    /**
+     * METHOD FOR GETTING IP FROM USER
+     */
     private fun getIp() {
         //custom dialog layout
         val dialogBinding = DataBindingUtil.inflate<IpDialogLayoutBinding>(
@@ -52,6 +55,9 @@ class MainActivity : AppCompatActivity() {
             }.setCancelable(false).show()
     }
 
+    /**
+     * METHOD FOR CONNECTING TO SERVER
+     */
     private fun connectToServer() {
         socketThread = Thread {
             try {
@@ -60,38 +66,64 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
                 }
 
+                //start receiving image data
                 receiveImage()
 
             } catch (e: Exception) {
-                e.printStackTrace()
+                runOnUiThread {
+                    getIp()
+                    Toast.makeText(this, "Error Connecting to server", Toast.LENGTH_SHORT).show()
+                }
             }
         }
-        socketThread.start()
+        socketThread!!.start()
     }
 
+    /**
+     * METHOD FOR RECEIVING IMAGE DATA FROM SERVER AND DISPLAY ON IMAGE VIEW
+     */
     private fun receiveImage() {
+        //get data stream
         viewModel.dataInputStream = DataInputStream(viewModel.socket?.getInputStream())
         while (true) {
-            viewModel.size = viewModel.dataInputStream?.readInt()!!
-            viewModel.bytes = ByteArray(viewModel.size)
-            viewModel.dataInputStream?.readFully(viewModel.bytes)
-            val bitmap = BitmapFactory.decodeByteArray(viewModel.bytes, 0, viewModel.bytes!!.size)
-            runOnUiThread {
-                binding.imageView.setImageBitmap(bitmap)
+            try {
+                //image size
+                viewModel.size = viewModel.dataInputStream?.readInt()!!
+
+                //image bytes
+                viewModel.bytes = ByteArray(viewModel.size)
+                viewModel.dataInputStream?.readFully(viewModel.bytes)
+
+                //creating bitmap
+                val bitmap =
+                    BitmapFactory.decodeByteArray(viewModel.bytes, 0, viewModel.bytes!!.size)
+                runOnUiThread {
+                    //set image on imageview
+                    binding.imageView.setImageBitmap(bitmap)
+                }
+                Thread.sleep(1)
+            } catch (e: Exception) {
+                runOnUiThread {
+                    getIp()
+                    Toast.makeText(this, "Server Disconnected", Toast.LENGTH_SHORT).show()
+                }
+                break;
             }
-            Thread.sleep(1)
         }
     }
 
+    /**
+     * CLOSE SOCKET CONNECTION AND SOCKET THREAD WHEN USER CLOSES THE APP
+     */
     override fun onStop() {
         super.onStop()
-        socketThread.join()
+        socketThread!!.join()
         viewModel.socket!!.close()
         viewModel.dataInputStream!!.close()
     }
 
     override fun onStart() {
         super.onStart()
-        connectToServer()
+        if (socketThread != null) socketThread!!.start()
     }
 }
